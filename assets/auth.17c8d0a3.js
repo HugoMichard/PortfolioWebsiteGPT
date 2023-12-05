1b3361b05d0ccdd964c8a35e20436a0def36a6ef
HugoMichard <hugomichard@yahoo.com>
2023-12-05 14:25:04 +0100

    Updates

--- assets/auth.17c8d0a3.js
+++
@@ -1,1 +0,0 @@
-import{aO as t}from"./entry.8e8a707b.js";const r=t((e,o)=>{});export{r as default};
---
+++ assets/auth.f0e2616d.js
@@ -0,0 +1,1 @@
+import{aO as t}from"./entry.8cc208bc.js";const r=t((e,o)=>{});export{r as default};
