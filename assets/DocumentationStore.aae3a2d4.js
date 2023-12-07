792df343aab9b50d8e49b92a3f9299313ade7a9e
HugoMichard <hugomichard@yahoo.com>
2023-12-07 14:05:43 +0100

    Updates

--- assets/DocumentationStore.aae3a2d4.js
+++
@@ -1,1 +0,0 @@
-import{aS as n}from"./entry.be506370.js";const i=n("DocumentationStore",{state:()=>({viewedPageMarkdown:"",pathToFile:""}),actions:{async getPageMarkdown(e){const o=await fetch("/PortfolioWebsiteGPT"+e+".md").then(function(t){return t.text()}).then(function(t){return t});this.viewedPageMarkdown=o,this.pathToFile=e}}});export{i as u};
---
+++ assets/auth.cebf8b13.js
@@ -0,0 +1,1 @@
+import{aO as t}from"./entry.4bf604f5.js";const r=t((e,o)=>{});export{r as default};
